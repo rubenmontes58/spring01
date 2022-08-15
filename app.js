09c957f06568254b1bb51c3042a470bf67abb907
RUBEN MONTES <rubenmontes58@gmail.com>
2022-08-15 17:26:19 -0300

    falta el css

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 const morgan = require('morgan');
-app.use(morgan('dev'));
+
 const hbs = require('hbs');
 const path = require('path');
 path.join(__dirname, 'views');
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 const port = process.env.PORT || 3000;
 const mysql = require('mysql2');
 const routerHome = require('./routes/router-home');
-const { all } = require('./routes/router-home');
+
 const { default: knex } = require('knex');
 
 
@@ -25,7 +21,11 @@ const { default: knex } = require('knex');
 
 
 //middelwares
-
+app.use(morgan('dev'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -34,33 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.use('/', routerHome);
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
